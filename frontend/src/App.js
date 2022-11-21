37531ff6f94ebd8e851f2bdadbda3a5373284956
AviralSinghPal <aviralpal00007@gmail.com>
2022-11-21 22:58:49 +0530

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Form } from './components/form';
+import { UserList } from './components/userList';
+
+function App() {
+  return <div >
+      <Form/>
+      <UserList/>
+    </div>
+}
+
+
+
+export default App;
